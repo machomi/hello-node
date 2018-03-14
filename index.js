315ff54f27fccc901463aec48cd1c93f74dd0681
Michal Machowski <machowski.michal@gmail.com>
2018-03-14 12:59:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 var counter = 0
-app.get('/', (req, res) => res.send('Hello World! Nb of visits is ' + ++counter))
+app.get('/', (req, res) => res.send('Hello World! This is version 2.0. Nb of visits is ' + ++counter))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
